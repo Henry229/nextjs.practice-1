5beffb6abd7771899f907a0776f48120437d0882
Henry <chunhyungjoon@gmail.com>
2023-05-22 16:17:29 +1000

    add Link

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import Link from 'next/link';
+
+const product = ['pants', 'shirts', 'shoes'];
 
 export default function ProductsPage() {
   return (
     <div>
       <h1>제품 소개 페이지!</h1>
+      <ul>
+        {product.map((product, index) => (
+          <li key={index}>
+            <Link href={`products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
