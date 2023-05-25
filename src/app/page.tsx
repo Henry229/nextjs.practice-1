88fce27cc00110dc0a405fabfadd82c5c524785e
Henry Chun <chunhyungjoon@gmail.com>
2023-05-25 21:29:49 +1000

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,12 @@ export default function Home() {
   return (
     <>
       <h1>홈페이지다!</h1>
+      <Image
+        src=' https://images.unsplash.com/photo-1441986300917-64674bd600d8'
+        alt='shop'
+        width={400}
+        height={400}
+      />
       <Counter />
     </>
   );
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getProduct, getProducts } from '@/service/products';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 export const revalidate = 3; // 3초마다 새로고침 ISR 페이지로 만듬
@@ -28,6 +29,12 @@ export default async function ProductPage({ params: { slug } }: Props) {
   return (
     <div>
       <h1>{product.name} 제품 설명 페이지</h1>
+      <Image
+        src={`/images/${product.image}`}
+        alt={product.name}
+        width='300'
+        height='300'
+      />
     </div>
   );
 }
